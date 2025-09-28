2913898f36c82ca8e5b45cf75f76e342fd14f179
kiran <kiranpallathu1003@gmail.com>
2025-09-28 14:21:27 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:may_dice_app/DiceScreen.dart';
+
+void main(){
+
+  runApp(MyWidget());
+
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Dicescreen(),
+    );
+  }
+}
